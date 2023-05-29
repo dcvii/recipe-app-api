bf36532e00a8b7a669d54fe906c177de462057f2
Michael Bowen <mdcbowen@gmail.com>
2023-05-29 15:43:29 -0700

    through TDD on database

--- app/app/test.py
+++ app/app/test.py
@@ -1,20 +1,15 @@
-# sample tests
-
 from django.test import SimpleTestCase
 from app import calc
 
 class CalcTest(SimpleTestCase):
-    # test the add function
 
+
+    # test adding numbers together
     def test_add(self):
-        #test adding numbers together
-        result = calc.add(3,8)
+        result = calc.add(3, 8)
         self.assertEqual(result, 11)
 
     # test the subtract function
     def test_subtract(self):
-        # test subtracting numbers together
-        result = calc.subtract(5,11)
+        result = calc.subtract(5, 11)
         self.assertEqual(result, 6)
-
-        
